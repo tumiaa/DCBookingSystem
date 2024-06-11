2a58e134d0539ab6d323097782a22c12335398a2
tumiaa <169124828+tumiaa@users.noreply.github.com>
2024-06-11 05:11:49 +0000

    added template to yml file

---
+++ setup.nuxt.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+// import { getConfig } from './config';
+import { useRuntimeConfig } from "#app";
+import App from "@/app.vue";
+
+//TODO: make it so that only this test uses the env file.
+describe("Environment Variables", () => {
+  it("should get the correct values from environment variables", async () => {
+    const config = useRuntimeConfig();
+    expect(config.NUXT_AAD_ENDPOINT_BASE).toBeDefined();
+    expect(config.NUXT_CLIENT_ID).toBeDefined();
+    expect(config.NUXT_CLIENT_SECRET).toBeDefined();
+    expect(config.NUXT_TENANT_ID).toBeDefined();
+    expect(config.public.NUXT_PUBLIC_ENV_URL).toBeDefined();
+
+    expect(config.NUXT_AAD_ENDPOINT_BASE).toMatch(/(https:\/\/(.*)\.com\/)/);
+    expect(config.NUXT_CLIENT_ID).not.toEqual("");
+    expect(config.NUXT_CLIENT_SECRET).not.toEqual("");
+    expect(config.NUXT_TENANT_ID).not.toEqual("");
+    expect(config.public.NUXT_PUBLIC_ENV_URL).toMatch(
+      /(https:\/\/(.*)\.com\/api\/)/
+    );
+
+  });
+});
